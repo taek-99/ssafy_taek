4caf9235c6c83d4c0ad42de1e52aca7e8eda2747
taek99 <gunha1215@gmail.com>
2026-01-19 13:23:00 +0900

    tt

---
+++ algorithm/swea25430/solution.py
@@ -0,0 +1,20 @@
+
+house_list = {}
+heap_list = {}
+
+
+def init(N, K, sBuilding, eBuilding, mDistance):
+	global house_list, heap_list
+
+	house_list = {x: {} for x in range(N)}	
+	print(house_list)
+	return
+
+
+
+def add(sBuilding, eBuilding, mDistance):
+	return
+
+
+def calculate(M, mCoffee, P, mBakery, R):
+	return 0
